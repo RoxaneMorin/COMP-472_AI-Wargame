177462cace07b52273767efde086e41496caa343
Roxane Morin <41652942+RoxaneMorin@users.noreply.github.com>
2023-09-21 11:30:31 -0400

    Split the skeleton code in a few files.

---
+++ ai_wargame_config.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+import argparse
+import copy
+from datetime import datetime
+from enum import Enum
+from dataclasses import dataclass, field
+from time import sleep
+from typing import Tuple, TypeVar, Type, Iterable, ClassVar
+import random
+import requests
+
+
+# maximum and minimum values for our heuristic scores (usually represents an end of game condition)
+MAX_HEURISTIC_SCORE = 2000000000
+MIN_HEURISTIC_SCORE = -2000000000
+
+class UnitType(Enum):
+    """Every unit type."""
+    AI = 0
+    Tech = 1
+    Virus = 2
+    Program = 3
+    Firewall = 4
+
+class Player(Enum):
+    """The 2 players."""
+    Attacker = 0
+    Defender = 1
+
+    def next(self) -> Player:
+        """The next (other) player."""
+        if self is Player.Attacker:
+            return Player.Defender
+        else:
+            return Player.Attacker
+
+class GameType(Enum):
+    AttackerVsDefender = 0
+    AttackerVsComp = 1
+    CompVsDefender = 2
+    CompVsComp = 3
+
+##############################################################################################################
+
+@dataclass(slots=True)
+class Options:
+    """Representation of the game options."""
+    dim: int = 5
+    max_depth : int | None = 4
+    min_depth : int | None = 2
+    max_time : float | None = 5.0
+    game_type : GameType = GameType.AttackerVsDefender
+    alpha_beta : bool = True
+    max_turns : int | None = 100
+    randomize_moves : bool = True
+    broker : str | None = None
+
+##############################################################################################################
+
+@dataclass(slots=True)
+class Stats:
+    """Representation of the global game statistics."""
+    evaluations_per_depth : dict[int,int] = field(default_factory=dict)
+    total_seconds: float = 0.0
